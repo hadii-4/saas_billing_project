fe8147db139aec73879ff348220c96efe4a7d657
haddi-1 <abbasiabdulhadi060@gmail.com>
2026-05-19 07:15:14 +0500

    fix: update frontend API baseURL to live Vercel link

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,29 @@
+// import axios from 'axios';
+
+// const API = axios.create({
+//     // baseURL: 'http://localhost:3000/api', // Apna backend URL check karo
+
+//     https://saas-billing-project-pj8p.vercel.app/api,
+// });
+
+// // Interceptor yahan ayega
+// API.interceptors.request.use((req) => {
+//     const token = localStorage.getItem('token');
+//     if (token) {
+//         req.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return req;
+// });
+
+// export default API;
+
+
+
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:3000/api', // Apna backend URL check karo
+    // Ab localhost ke bajaye live Vercel URL base URL ban gaya hai
+    baseURL: 'https://saas-billing-project-pj8p.vercel.app/api',
 });
 
 // Interceptor yahan ayega
